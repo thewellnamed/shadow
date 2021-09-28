aebac1d916ca53d6cebc390c9663983e154bd1fd
thewellnamed <matthew.kauffman@gmail.com>
2021-09-28 08:36:02 -0600

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home.component';
 
 @NgModule({
@@ -14,7 +20,12 @@ import { HomeComponent } from './home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public encounters: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.encounters = [
+      { value: 1, label: 'Hydross the Unstable #1 (Wipe, 9:14pm)' },
+      { value: 2, label: 'Hydross the Unstable #2 (Kill, 9:27pm)' },
+      { value: 3, label: 'The Lurker Below (Kill, 9:47pm)' }
+    ];
   }
 }
