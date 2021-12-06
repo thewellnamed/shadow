fe6c58bd9df3b213ad2665ad1c9421adb94024d1
thewellnamed <matthew.kauffman@gmail.com>
2021-12-05 17:11:07 -0700

    add statsByTick

--- src/app/logs/models/spell-data.ts
+++ src/app/logs/models/spell-data.ts
@@ -8,158 +8,140 @@ export enum DamageType {
   AOE
 }
 
+const SPELL_DEFAULTS: Partial<ISpellData> = {
+  maxDamageInstances: 0,
+  maxDuration: 0,
+  cooldown: 0,
+  statsByTick: false
+};
+
+function spellData(params: Partial<ISpellData> = {}): ISpellData {
+  return Object.assign({}, SPELL_DEFAULTS, params) as ISpellData;
+}
+
 export const SpellData: {[spellId: number]: ISpellData} = {
-  [SpellId.ADAMANTITE_GRENDADE]: {
+  [SpellId.ADAMANTITE_GRENDADE]: spellData({
     damageType: DamageType.AOE,
     maxDamageInstances: 20,
-    maxDuration: 0,
-    cooldown: 0
-  },
+  }),
 
-  [SpellId.DEATH]: {
+  [SpellId.DEATH]: spellData({
     damageType: DamageType.DIRECT,
     maxDamageInstances: 1,
-    maxDuration: 0,
     cooldown: 12
-  },
+  }),
 
-  [SpellId.DENSE_DYNAMITE]: {
+  [SpellId.DENSE_DYNAMITE]: spellData({
     damageType: DamageType.AOE,
     maxDamageInstances: 20,
-    maxDuration: 0,
-    cooldown: 0
-  },
+  }),
 
-  [SpellId.DESPERATE_PRAYER]: {
-    damageType: DamageType.NONE,
-    maxDamageInstances: 0,
-    maxDuration: 0,
-    cooldown: 0
-  },
+  [SpellId.DESPERATE_PRAYER]: spellData({
+    damageType: DamageType.NONE
+  }),
 
-  [SpellId.DEVOURING_PLAGUE]: {
+  [SpellId.DEVOURING_PLAGUE]: spellData({
     damageType: DamageType.DOT,
     maxDamageInstances: 8,
     maxDuration: 24,
     cooldown: 180
-  },
+  }),
 
-  [SpellId.FADE]: {
+  [SpellId.FADE]: spellData({
     damageType: DamageType.NONE,
-    maxDamageInstances: 0,
     maxDuration: 10,
     cooldown: 30
-  },
+  }),
 
-  [SpellId.FEAR_WARD]: {
+  [SpellId.FEAR_WARD]: spellData({
     damageType: DamageType.NONE,
-    maxDamageInstances: 0,
     maxDuration: 180,
     cooldown: 180
-  },
+  }),
 
-  [SpellId.GOBLIN_LAND_MINE]: {
+  [SpellId.GOBLIN_LAND_MINE]: spellData({
     damageType: DamageType.NONE,
-    maxDamageInstances: 0,
-    maxDuration: 0,
-    cooldown: 0
-  },
+  }),
 
-  [SpellId.GOBLIN_SAPPER]: {
+  [SpellId.GOBLIN_SAPPER]: spellData({
     damageType: DamageType.AOE,
     maxDamageInstances: 20,
-    maxDuration: 0,
-    cooldown: 0
-  },
+  }),
 
-  [SpellId.MELEE]: {
+  [SpellId.MELEE]: spellData({
     damageType: DamageType.DIRECT,
-    maxDamageInstances: 0,
-    maxDuration: 0,
-    cooldown: 0
-  },
+  }),
 
-  [SpellId.MIND_BLAST]: {
+  [SpellId.MIND_BLAST]: spellData({
     damageType: DamageType.DIRECT,
     maxDamageInstances: 1,
-    maxDuration: 0,
     cooldown: 5.5
-  },
+  }),
 
-  [SpellId.MIND_FLAY]: {
+  [SpellId.MIND_FLAY]: spellData({
     damageType: DamageType.CHANNEL,
     maxDamageInstances: 3,
     maxDuration: 3,
-    cooldown: 0
-  },
+    statsByTick: true
+  }),
 
-  [SpellId.NETHERWEAVE_NET]: {
+  [SpellId.NETHERWEAVE_NET]: spellData({
     damageType: DamageType.NONE,
-    maxDamageInstances: 0,
-    maxDuration: 0,
     cooldown: 60
-  },
+  }),
 
-  [SpellId.PAIN]: {
+  [SpellId.PAIN]: spellData({
     damageType: DamageType.DOT,
     maxDamageInstances: 8,
     maxDuration: 24,
-    cooldown: 0
-  },
+  }),
 
-  [SpellId.SHADOW_FIEND]: {
+  [SpellId.SHADOW_FIEND]: spellData({
     damageType: DamageType.DIRECT,
     maxDamageInstances: 30,
     maxDuration: 15,
     cooldown: 300
-  },
+  }),
 
-  [SpellId.SHIELD]: {
+  [SpellId.SHIELD]: spellData({
     damageType: DamageType.NONE,
-    maxDamageInstances: 0,
     maxDuration: 30,
     cooldown: 4
-  },
+  }),
 
-  [SpellId.STARSHARDS]: {
+  [SpellId.STARSHARDS]: spellData({
     damageType: DamageType.DOT,
     maxDamageInstances: 5,
     maxDuration: 15,
     cooldown: 30
-  },
+  }),
 
-  [SpellId.SUPER_SAPPER]: {
+  [SpellId.SUPER_SAPPER]: spellData({
     damageType: DamageType.AOE,
     maxDamageInstances: 20,
-    maxDuration: 0,
-    cooldown: 0
-  },
+  }),
 
-  [SpellId.THORNLING]: {
+  [SpellId.THORNLING]: spellData({
     damageType: DamageType.NONE,
-    maxDamageInstances: 0,
-    maxDuration: 10,
-    cooldown: 0
-  },
+  }),
 
-  [SpellId.VAMPIRIC_EMBRACE]: {
+  [SpellId.VAMPIRIC_EMBRACE]: spellData({
     damageType: DamageType.NONE,
-    maxDamageInstances: 0,
     maxDuration: 60,
     cooldown: 10
-  },
+  }),
 
-  [SpellId.VAMPIRIC_TOUCH]: {
+  [SpellId.VAMPIRIC_TOUCH]: spellData({
     damageType: DamageType.DOT,
     maxDamageInstances: 5,
-    maxDuration: 15,
-    cooldown: 0
-  }
-}
+    maxDuration: 15
+  })
+};
 
 export interface ISpellData {
   damageType: DamageType;
   maxDamageInstances: number;
   maxDuration: number;
   cooldown: number;
+  statsByTick: boolean;
 }
